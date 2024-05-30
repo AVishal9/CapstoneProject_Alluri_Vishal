14bbf3b330e026f0fc8dbe98a2b71b528afe7d97
AVishal9 <vishal.alluri@stud.unilu.ch>
2024-05-30 18:49:37 +0200

    Better querying for Swiss source

--- scripts/capstone_initial.R
+++ scripts/capstone_initial.R
@@ -4,36 +4,40 @@ library(dplyr)
 
 api_token <- "CYSeowIHCwzUgoD4M0K9vr6KKQtjBTYv6XbC5cPZ"
 
-fetch_and_store_articles <- function(search_term = "healthcare+politics", 
-                                     num_articles = 10, 
-                                     start_date = "2023-09-01", 
+fetch_and_store_articles <- function(search_term = "healthcare", 
+                                     num_requests = 20, 
+                                     num_articles_per_request = 10, 
+                                     start_date = "2023-07-01", 
                                      end_date = "2023-10-21") {
   
   url <- "https://api.thenewsapi.com/v1/news/all"
   
+  # Swiss domains list (example)
+  swiss_domains <- "swissinfo.ch"
+  
   articles_df <- data.frame()
   
-  for (page_num in 1:ceiling(num_articles/100)) {
+  for (page_num in 1:num_requests) {
     params <- list(
       api_token = api_token,
       search = search_term,
       language = "en",
-      locale = "ch",
+      domains = swiss_domains,
       published_after = start_date,
       published_before = end_date,
-      limit = min(100, num_articles - (page_num - 1) * 100),
+      limit = num_articles_per_request,
       page = page_num
     )
     
     response <- GET(url, query = params)
     
     if (status_code(response) == 200) {
-      # Read the response body as raw text
-      response_text <- rawToChar(response$content)
+      # Read the response body as text
+      response_text <- content(response, as = "text")
       data <- fromJSON(response_text)
       
       if (length(data$data) > 0) {
-        articles_df <- rbind(articles_df, data$data)
+        articles_df <- bind_rows(articles_df, data$data)
       } else {
         break
       }
@@ -50,5 +54,8 @@ fetch_and_store_articles <- function(search_term = "healthcare+politics",
 news_articles <- fetch_and_store_articles()
 
 if (!is.null(news_articles)) {
-  print(news_articles) 
+  print(news_articles)
 }
+
+
+
