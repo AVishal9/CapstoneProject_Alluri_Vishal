f6d673c6da93c9c961bab9413cdda612ee495928
AVishal9 <vishal.alluri@stud.unilu.ch>
2024-05-30 20:50:52 +0200

    Swiss news data for 3 months - Classified based on 'worries'

--- scripts/capstone_initial.R
+++ scripts/capstone_initial.R
@@ -2,17 +2,17 @@ library(httr)
 library(jsonlite)
 library(dplyr)
 
-api_token <- "CYSeowIHCwzUgoD4M0K9vr6KKQtjBTYv6XbC5cPZ"
+api_token <- Sys.getenv("NEWS_API_KEY")
 
-fetch_and_store_articles <- function(search_term = "healthcare", 
-                                     num_requests = 20, 
-                                     num_articles_per_request = 10, 
-                                     start_date = "2023-07-01", 
+fetch_and_store_articles <- function(search_term, worry_label, 
+                                     num_requests = 10, 
+                                     num_articles_per_request = 25, #max no. the api allows
+                                     start_date = "2023-07-20", 
                                      end_date = "2023-10-21") {
   
   url <- "https://api.thenewsapi.com/v1/news/all"
   
-  # Swiss domains list (example)
+  # Swiss domains list 
   swiss_domains <- "swissinfo.ch"
   
   articles_df <- data.frame()
@@ -25,6 +25,7 @@ fetch_and_store_articles <- function(search_term = "healthcare",
       domains = swiss_domains,
       published_after = start_date,
       published_before = end_date,
+      #categories = "politics",
       limit = num_articles_per_request,
       page = page_num
     )
@@ -37,7 +38,9 @@ fetch_and_store_articles <- function(search_term = "healthcare",
       data <- fromJSON(response_text)
       
       if (length(data$data) > 0) {
-        articles_df <- bind_rows(articles_df, data$data)
+        df <- data.frame(data$data)
+        df$worries <- worry_label
+        articles_df <- bind_rows(articles_df, df)
       } else {
         break
       }
@@ -50,11 +53,36 @@ fetch_and_store_articles <- function(search_term = "healthcare",
   return(articles_df) 
 }
 
-# Fetch and store articles
-news_articles <- fetch_and_store_articles()
+search_terms <- list(
+  health = '"healthcare" | "health insurance"',
+  environment = '"environment" + "protection" | "climate change"',
+  retirement = '"AHV" | "retirement" + "provision"',
+  eu_relations = 'EU',
+  energy = '"Energy" + "issues"',
+  immigration = '"Foreigners" | "Immigration"',
+  inflation = '"Inflation"',
+  housing = '"housing + costs" | "rent"',
+  refugees = '"Refugees"',
+  social_security = '"Social" + "security"'
+)
 
-if (!is.null(news_articles)) {
-  print(news_articles)
+
+# Fetch and store articles
+all_articles <- data.frame()
+
+for (worry_label in names(search_terms)) {
+  search_term <- search_terms[[worry_label]]
+  articles <- fetch_and_store_articles(search_term, worry_label)
+  if (!is.null(articles)) {
+    all_articles <- bind_rows(all_articles, articles)
+  }
+}
+
+
+if (nrow(all_articles) > 0) {
+  print(all_articles)
+} else {
+  print("No articles found")
 }
 
 
